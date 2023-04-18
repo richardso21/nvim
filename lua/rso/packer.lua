d861d261323456792b902422a3fa67fdc2f0a5c6
Richard So <richardso2021@gmail.com>
2023-04-18 01:49:41 -0400

    zen mode

--- lua/rso/packer.lua
+++ lua/rso/packer.lua
@@ -1,7 +1,6 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require("packer").startup(function(use)
-
   -- >>> CORE >>>
 
   -- Packer can manage itself
@@ -10,7 +9,7 @@ return require("packer").startup(function(use)
   -- fuzzy finding
   use {
     "nvim-telescope/telescope.nvim", tag = "0.1.0",
-    requires = { {"nvim-lua/plenary.nvim"} }
+    requires = { { "nvim-lua/plenary.nvim" } }
   }
 
   -- color scheme
@@ -25,7 +24,7 @@ return require("packer").startup(function(use)
   -- tree sitter
   use {
     "nvim-treesitter/nvim-treesitter",
-    { run = ":TSUpdate"}
+    { run = ":TSUpdate" }
   }
 
   -- file tree
@@ -55,72 +54,75 @@ return require("packer").startup(function(use)
     branch = "v2.x",
     requires = {
       -- LSP Support
-      {"neovim/nvim-lspconfig"},             -- Required
-      {                                      -- Optional
-      "williamboman/mason.nvim",
-      run = function()
-        ---@diagnostic disable-next-line: param-type-mismatch
-        pcall(vim.cmd, "MasonUpdate")
-      end,
-    },
-    {"williamboman/mason-lspconfig.nvim"}, -- Optional
+      { "neovim/nvim-lspconfig" }, -- Required
+      {
+        -- Optional
+        "williamboman/mason.nvim",
+        run = function()
+          ---@diagnostic disable-next-line: param-type-mismatch
+          pcall(vim.cmd, "MasonUpdate")
+        end,
+      },
+      { "williamboman/mason-lspconfig.nvim" }, -- Optional
 
-    -- Autocompletion
-    {"hrsh7th/nvim-cmp"},     -- Required
-    {"hrsh7th/cmp-nvim-lsp"}, -- Required
-    {"L3MON4D3/LuaSnip"},     -- Required
-  }
-}
-
-
--- >>> MISC >>>
-
--- debugger
-use {
-  "mfussenegger/nvim-dap",
-  "jay-babu/mason-nvim-dap.nvim"
-}
-
--- commenting
-use "numToStr/Comment.nvim"
-
--- smooth scrolling
-use "karb94/neoscroll.nvim"
-
--- lsp progress 
-use "j-hui/fidget.nvim"
-
--- indent guides
-use "lukas-reineke/indent-blankline.nvim"
-
--- git
-use "tpope/vim-fugitive"
-
--- undotree
-use "mbbill/undotree"
-
--- which key
-use {
-  "folke/which-key.nvim",
-  config = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-    require("which-key").setup {}
-  end
-}
-
--- save sessions
-use {
-  "rmagatti/auto-session",
-  config = function()
-    require("auto-session").setup {
-      log_level = "error",
-      auto_session_suppress_dirs = { "~/", "~/Downloads", "/"},
+      -- Autocompletion
+      { "hrsh7th/nvim-cmp" },     -- Required
+      { "hrsh7th/cmp-nvim-lsp" }, -- Required
+      { "L3MON4D3/LuaSnip" },     -- Required
     }
-  end
-}
+  }
 
--- dim inactive windows
-use "levouh/tint.nvim"
 
+  -- >>> MISC >>>
+
+  -- debugger
+  use {
+    "mfussenegger/nvim-dap",
+    "jay-babu/mason-nvim-dap.nvim"
+  }
+
+  -- commenting
+  use "numToStr/Comment.nvim"
+
+  -- smooth scrolling
+  use "karb94/neoscroll.nvim"
+
+  -- lsp progress
+  use "j-hui/fidget.nvim"
+
+  -- indent guides
+  use "lukas-reineke/indent-blankline.nvim"
+
+  -- git
+  use "tpope/vim-fugitive"
+
+  -- undotree
+  use "mbbill/undotree"
+
+  -- which key
+  use {
+    "folke/which-key.nvim",
+    config = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+      require("which-key").setup {}
+    end
+  }
+
+  -- save sessions
+  use {
+    "rmagatti/auto-session",
+    config = function()
+      require("auto-session").setup {
+        log_level = "error",
+        auto_session_suppress_dirs = { "~/", "~/Downloads", "/" },
+      }
+    end
+  }
+
+  -- dim inactive windows
+  use "levouh/tint.nvim"
+
+  -- zen mode
+  use "folke/zen-mode.nvim"
 end)
